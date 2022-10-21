39b782019de7f7ce6995d25db8b799ecd0ce5da0
Agiratech-Maadhan <madhan.k@agiratech.com>
2022-10-21 12:26:48 +0530

    mixins with classes

--- classes.dart
+++ classes.dart
@@ -1,10 +1,17 @@
+mixin Agility {
+  var speed = 10;
+  void sitDown() {
+    print('Sitting down');
+  }
+}
+
 class Mammal {
   void breathe() {
     print('Breathe in ... breathe out ...');
   }
 }
 
-class Person extends Mammal {
+class Person extends Mammal with Agility {
   String name;
   int age;
   Person(this.name, this.age);
@@ -14,4 +21,6 @@ void main() {
   final pers = Person('Max ', 30);
   print(pers.name);
   pers.breathe();
+  print(pers.speed);
+  pers.sitDown();
 }
